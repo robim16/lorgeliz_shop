9ffdb69a02b4e6e3e2c85b359ee3277d63c0b1f9
Carlos Arteaga <carteagajimenez@gmail.com>
2022-10-02 11:02:33 -0500

    agregar subtotal y envío en admin pedido pdf

--- resources/views/admin/pdf/pedido.blade.php
+++ resources/views/admin/pdf/pedido.blade.php
@@ -167,7 +167,16 @@
             </tbody>
         </table>
     </div> 
-    <div class="izquierda">
+    {{-- <div class="izquierda"> --}}
+    <div class="">
+        <p><strong>Subtotal: </strong>${{ floatval($producto->venta->subtotal)}}</p>
+    </div> 
+
+    <div class="">
+        <p><strong>Envío: </strong>${{ floatval($producto->venta->envio)}}</p>
+    </div> 
+
+    <div class="">
         <p><strong>Total pedido: </strong>${{ floatval($producto->venta->valor)}}</p>
     </div> 
 </body>
