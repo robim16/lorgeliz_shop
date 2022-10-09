a225d95408d2ba41bce29873407952d99d918ceb
Carlos Arteaga <carteagajimenez@gmail.com>
2022-10-09 14:20:22 -0500

    logs en form contacto

--- app/Http/Controllers/ContactoController.php
+++ app/Http/Controllers/ContactoController.php
@@ -6,9 +6,11 @@ use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\ClientToAdminMail;
+use Illuminate\Support\Facades\Log;
 
 class ContactoController extends Controller
 {
+    
     /**
      * Create a new controller instance.
      *
@@ -19,6 +21,7 @@ class ContactoController extends Controller
         $this->middleware('auth');
     }
 
+
     public function contacto()
     {
         return view('user.contact');
@@ -26,17 +29,25 @@ class ContactoController extends Controller
 
     public function sendMail(Request $request)
     {
-        $admin = User::where('role_id', 2)->first();
-        $user = auth()->user();
-    
-        $details = [
-            'title' => 'Has recibido un email de un cliente',
-            'cliente' => $user->nombres.' '.$user->apellidos,
-            'mensaje' => $request->mensaje,
-            'url' => url('/admin/clientes/'. $user->cliente->id),
-        ];
 
-        Mail::to($admin->email)->send(new ClientToAdminMail($details));
+        try {
+           
+            $admin = User::where('role_id', 2)->first();
+            $user = auth()->user();
+        
+            $details = [
+                'title' => 'Has recibido un email de un cliente',
+                'cliente' => $user->nombres.' '.$user->apellidos,
+                'mensaje' => $request->mensaje,
+                'url' => url('/admin/clientes/'. $user->cliente->id),
+            ];
+    
+            Mail::to($admin->email)->send(new ClientToAdminMail($details));
+
+        } catch (\Exception $e) {
+
+            Log::debug('Error enviando el email del formulario de contacto'.'Error:'.json_encode($e));
+        }
 
         // return new ClientToAdminMail($details);
     }
