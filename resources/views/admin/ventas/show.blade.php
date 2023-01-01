4f5c4a3ee48f8f1afa2ab620c234c9f9943c61e5
Carlos Arteaga <carteagajimenez@gmail.com>
2023-01-01 13:37:12 -0500

    cambios en show ventas

--- resources/views/admin/ventas/show.blade.php
+++ resources/views/admin/ventas/show.blade.php
@@ -31,7 +31,135 @@
     <div id="venta_show">
 
         <div class="row">
-
+    
+            {{-- <div class="col-12"> --}}
+                {{-- <div class="card"> --}}
+                    {{-- <div class="card-header">
+                        <h3 class="card-title">Detalle de la venta</h3>
+    
+                        <div class="card-tools">
+    
+                            <form>
+                                <div class="input-group input-group-sm" style="width: 150px;">
+                                    <input type="text" name="nombre" class="form-control float-right" placeholder="Buscar"
+                                    value="{{ request()->get('nombre') }}">
+    
+                                    <div class="input-group-append">
+                                        <button type="submit" class="btn btn-default"><i class="fas fa-search"></i></button>
+                                    </div>
+                                </div>
+                            </form>
+    
+                        </div>
+                    </div> --}}
+                    <!-- /.card-header -->
+                    {{-- <div class="card-body table-responsive p-0" style="height: 300px;"> --}}
+                        {{-- <table class="table table-hover table-striped"> --}}
+                            {{-- <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Fecha</th>
+                                    <th>Cliente</th>
+                                    <th>Factura</th>
+                                    <th>Valor</th>
+                                    <th>Pagos</th>
+                                    <th>Devoluciones</th>
+                                    <th>Saldo</th>
+                                    <th>Estado</th>
+                                    <th>Acciones</th>
+                                    <th colspan="9"></th>
+                                </tr>
+                            </thead> --}}
+                            {{--<tbody>--}}
+    
+                                {{-- <tr> --}}
+                                    {{-- <td>{{ $venta->id }}</td>
+                                    <td>{{ date('d/m/Y h:i:s A', strtotime($venta->fecha)) }}</td>
+                                    <td><a href="{{ route('cliente.show', $venta->cliente)}}">{{ $venta->nombres }} {{ $venta->apellidos}}</a></td>
+                                    <td>{{ ($venta->prefijo) }}{{ ($venta->consecutivo) }}</td>
+                                    <td>${{ floatval($venta->valor) }}</td>
+                                    <td>${{ floatval($venta->saldo) }}</td>
+                                    <td>
+                                        @if ($venta->estado == 1)
+                                        {{ "Pagada" }}
+                                        @endif
+                                        @if ($venta->estado == 2)
+                                        {{ "Con saldo" }}
+                                        @endif
+                                        @if ($venta->estado == 3)
+                                        {{ "Anulada" }}
+                                        @endif
+                                    </td>
+                                    @if ($venta->estado == 2 & $venta->saldo > 0)
+                                    <td>
+                                        <a class="btn btn-primary" href="" title="registrar pago" data-toggle="modal"
+                                            data-target="#modal" id="pay">
+                                            <i class="fas fa-money-bill-wave"></i>
+                                        </a>
+                                    </td>
+                                    @endif
+                                    <td><a href="" class="btn btn-success" title="imprimir factura" 
+                                            v-on:click.prevent="facturaVenta({{$venta->id}})">
+                                            <i class="fas fa-print"></i>
+                                        </a>
+                                    </td>
+                                    <td><a href="{{ route('admin.pedidos.show', $venta->pedido->id)}}" class="btn btn-info"
+                                            title="ver pedido"><i class="fas fa-shopping-cart"></i></a>
+                                    </td> --}}
+    
+                                    {{--<td>{{ $venta->id }}</td>
+                                    <td>{{ date('d/m/Y h:i:s A', strtotime($venta->fecha)) }}</td>
+                                    <td><a href="{{ route('cliente.show', $venta->cliente->id)}}">{{ $venta->cliente->user->nombres }} {{ $venta->cliente->user->apellidos}}</a></td>
+                                    <td>{{ ($venta->factura->prefijo) }}{{ ($venta->factura->consecutivo) }}</td>
+                                    <td>${{ floatval($venta->valor) }}</td>
+                                    <td>${{ $pagos[0]->total ? floatval($pagos[0]->total) : 0 }}</td>
+                                    <td>${{ floatval($valor_devolucion)}}</td>
+                                    <td>${{ floatval($venta->saldo) }}</td>
+                                    <td>
+                                        @if ($venta->estado == 1)
+                                            <span class="badge badge-success">
+                                            {{ "Pagada" }}
+                                            </span>
+                                        @endif
+                                        @if ($venta->estado == 2)
+                                            <span class="badge badge-warning">
+                                            {{ "Con saldo" }} 
+                                            </span>
+                                        @endif
+                                        @if ($venta->estado == 3)
+                                            <span class="badge badge-danger">
+                                            {{ "Anulada" }}
+                                            </span>
+                                        @endif
+                                    </td>
+                                    @if ($venta->estado == 2 & $venta->saldo > 0)
+                                    <td>
+                                        <a class="btn btn-primary" href="" title="registrar pago" data-toggle="modal"
+                                            data-target="#modal" id="pay">
+                                            <i class="fas fa-money-bill-wave"></i>
+                                        </a>
+                                    </td>
+                                    @endif
+                                    <td><a href="" class="btn btn-success" title="imprimir factura" 
+                                            @click.prevent="facturaVenta({{$venta->id}})">
+                                            <i class="fas fa-print"></i>
+                                        </a>
+                                    </td>
+                                    <td><a href="{{ route('admin.pedidos.show', $venta->pedido->id)}}" class="btn btn-info"
+                                            title="ver pedido"><i class="fas fa-shopping-cart"></i></a>
+                                    </td>
+    
+    
+                                </tr>--}}
+                                
+                            {{--</tbody>--}}
+                        {{-- </table> --}}
+                    
+                    {{-- </div> --}}
+                    <!-- /.card-body -->
+                {{-- </div> --}}
+                <!-- /.card -->
+            {{-- </div> --}}
     
             <div class="col-12">
                 <div class="card card-primary card-outline card-outline-tabs">
@@ -307,6 +435,7 @@
         </div>
         <!-- /.row -->
 
+
     
         <div class="modal fade" tabindex="-1" role="dialog"  id="modalPago" :class="{'mostrar' : modal}">
             <div class="modal-dialog" role="document">
@@ -335,7 +464,12 @@
                                 <label class="col-md-4 form-control-label" for="text-input">Valor del pago</label>
                                 <div class="col-md-8">
                                     <input type="number" name="valor" id="valor" class="form-control" value="" v-model="valor">
-                                    
+                                    {{-- @if($errors->has('valor'))
+                                        <small class="form-text text-danger">
+                                            {{ $errors->first('valor') }}
+                                        </small>
+                                    @endif --}}
+    
                                     <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
                                     <span class="text-danger">
                                         <strong id="valor-error"></strong>
