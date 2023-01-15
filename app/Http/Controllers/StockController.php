7afec320983c337fca0ec337f66f5ed16f66f70c
Carlos Arteaga <carteagajimenez@gmail.com>
2023-01-15 13:10:05 -0500

    verificar stock al realizar venta

--- app/Http/Controllers/StockController.php
+++ app/Http/Controllers/StockController.php
@@ -24,59 +24,54 @@ class StockController extends Controller
 
     
     public function verificarStock(Request $request)
-    {  
-        
+    {   
         //requiere auth
-        // if (!$request->ajax()) return redirect('/');
-        // $carrito = Carrito::where('cliente_id', auth()->user()->cliente->id)
-        // ->where('carritos.estado', '1')
-        // ->first(); // se obtiene el carrito del cliente
-        
-        // $productos = ProductoReferencia::join('carrito_producto', 'producto_referencia.id',
-        // 'carrito_producto.producto_referencia_id')
-        // ->join('carritos', 'carrito_producto.carrito_id', 'carritos.id')
-        // ->where('carritos.id', $carrito->id)
-        // ->get();
-
         if ( ! request()->ajax()) {
-            abort(401, 'Acceso denegado');
-        };
+			abort(401, 'Acceso denegado');
+		}
+        
 
         try {
+           
             
             $productos = CarritoProducto::whereHas('carrito', function (Builder $query) {
-            $query->where('estado', 1)
-            ->where('cliente_id', auth()->user()->cliente->id);
+                $query->where('estado', 1)
+               ->where('cliente_id', auth()->user()->cliente->id);
             })
             ->with('productoReferencia')
-            ->get();
+            ->get()
+            ->first(function ($producto) {
+                return $producto->cantidad > $producto->productoReferencia->stock;
+            });// si un producto está agotado o la cantidad supera el stock
 
-            
-            foreach ($productos as $producto) {
-                if ($producto->cantidad > $producto->productoReferencia->stock) {
-                    $response = ['data' => 'error'];
-                    return response()->json($response); // si un producto está agotado o la cantidad supera el stock
-                }
+
+            if ($productos) {
+
+                $response = ['data' => 'error'];
             }
-            
-            $response = ['data' => 'success'];
+
+            else {
+
+                $response = ['data' => 'success'];
+            }
+
             
             return response()->json($response);// se ejecuta antes de levantar el formulario de epayco
-
+           
+           
+            // foreach ($productos as $producto) {
+            //     if ($producto->cantidad > $producto->productoReferencia->stock) {
+            //         $response = ['data' => 'error'];
+            //         return response()->json($response); // si un producto está agotado o la cantidad supera el stock
+            //     }
+            // }
+    
+            // $response = ['data' => 'success'];
+    
         } catch (\Exception $e) {
-            
+
             Log::debug('Error verificando el stock'.'error:'.' '.json_encode($e));
         }
-
-
-
-
-        // foreach ($productos as $producto) {
-        //    if ($producto->cantidad > $producto->stock) {
-        //         $response = ['data' => 'error'];
-        //         return response()->json($response); // si un producto está agotado o la cantidad supera el stock
-        //    }
-        // }
 
     }
 }
