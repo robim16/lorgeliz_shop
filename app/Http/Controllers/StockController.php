8aec78be0e77482eaf63477d13358afbb4078d36
Carlos Arteaga <carteagajimenez@gmail.com>
2023-01-15 13:14:23 -0500

    verificar stock al realizar venta

--- app/Http/Controllers/StockController.php
+++ app/Http/Controllers/StockController.php
@@ -22,36 +22,52 @@ class StockController extends Controller
         
     }
 
+   
+
     public function verificarStock(Request $request)
     {   
         //requiere auth
-
+     
         if ( ! request()->ajax()) {
 			abort(401, 'Acceso denegado');
 		}
+        
 
         try {
            
 
             $productos = CarritoProducto::whereHas('carrito', function (Builder $query) {
                 $query->where('estado', 1)
-                ->where('cliente_id', auth()->user()->cliente->id);
-                })
-                ->with('productoReferencia')
-                ->get();
+               ->where('cliente_id', auth()->user()->cliente->id);
+            })
+            ->with('productoReferencia')
+            ->get()
+            ->first(function ($producto) {
+                return $producto->cantidad > $producto->productoReferencia->stock;
+            });// si un producto está agotado o la cantidad supera el stock
 
 
-            foreach ($productos as $producto) {
-                if ($producto->cantidad > $producto->productoReferencia->stock) {
-                    $response = ['data' => 'error'];
-                    return response()->json($response); // si un producto está agotado o la cantidad supera el stock
-                }
+            if ($productos) {
+                $response = ['data' => 'error'];
             }
 
-            $response = ['data' => 'success'];
+            else {
+                $response = ['data' => 'success'];
+            }
 
+            
             return response()->json($response);// se ejecuta antes de levantar el formulario de epayco
-
+           
+           
+            // foreach ($productos as $producto) {
+            //     if ($producto->cantidad > $producto->productoReferencia->stock) {
+            //         $response = ['data' => 'error'];
+            //         return response()->json($response); // si un producto está agotado o la cantidad supera el stock
+            //     }
+            // }
+    
+            // $response = ['data' => 'success'];
+    
         } catch (\Exception $e) {
 
             Log::debug('Error verificando el stock al realizar venta'.'Error:'.' '.json_encode($e));
