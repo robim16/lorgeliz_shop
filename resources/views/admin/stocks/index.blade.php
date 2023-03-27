054a523f417cd004b0fc563efb4441860fe6df48
Carlos Arteaga <carteagajimenez@gmail.com>
2023-03-27 13:38:09 -0500

    compilación actualizacion stock con vue

--- resources/views/admin/stocks/index.blade.php
+++ resources/views/admin/stocks/index.blade.php
@@ -37,10 +37,11 @@
                                 </form>
                             </div>
                         </div>
-                        <!-- /.card-header -->
+
                         <div class="card-body table-responsive p-0">
-                            <button type="button" id="new" class="m-2 float-right btn btn-primary" data-toggle="modal"
-                                data-target="#modalStock" @click.prevent="reset"><i class="far fa-plus-square"></i>
+                            <button type="button" id="new" class="m-2 float-right btn btn-primary"
+                                data-toggle="modal" data-target="#modalStock" @click.prevent="reset"><i
+                                    class="far fa-plus-square"></i>
                                 Nuevo</button>
                             <table class="table table-head-fixed">
                                 <thead>
@@ -56,60 +57,80 @@
                                 </thead>
                                 <tbody>
 
-                                {{-- @foreach ($productos as $producto)
 
-                                <tr>
-                                    <td>{{ $producto->id }}</td>
-                                    <td>{{ $producto->nombre }}</td>
-                                    <td> <a href="{{ route('producto.show', $producto->slug) }}"> 
-                                        @foreach (\App\Imagene::where('imageable_type', 'App\ColorProducto')->where('imageable_id', $producto->cop)->pluck('url', 'id')->take(1)
-                                        as $id => $imagen)    
-                                        <img src="{{ url('storage/' . $imagen) }}" alt="" style="height: 50px; width: 50px;" class="rounded-circle">
-                                        @endforeach
-                                        </a>
-                                    </td>
-                                    <td>{{ $producto->talla }}</td>
-                                    <td>{{ $producto->color }}</td>
-                                    <td>{{ $producto->stock }}</td>
-                                    <td><a href="" class="btn btn-success" data-toggle="modal" data-target="#modalStock" v-on:click.prevent="selectProducto({{$producto}})"> <i class="far fa-plus-square"></i></a></td>
-                                </tr>
-                                    
-                                @endforeach --}}
+                                    <tr v-for="producto in arrayProductos" :key="producto.id">
 
-                                    @foreach ($productos as $producto)
-                                        <tr>
-                                            <td>{{ $producto->colorProducto->producto->id }}</td>
-                                            <td>{{ $producto->colorProducto->producto->nombre }}</td>
-                                            <td> <a href="{{ route('productos.show', $producto->colorProducto->slug) }}">
-                                                    <img src="{{ url('storage/' . $producto->colorProducto->imagenes[0]->url) }}"
-                                                        alt="" style="height: 50px; width: 50px;" class="rounded-circle">
-                                                </a>
-                                            </td>
-                                            <td>{{ $producto->talla->nombre }}</td>
-                                            <td>{{ $producto->colorProducto->color->nombre }}</td>
-                                            <td>{{ $producto->stock }}</td>
-                                            <td>
-                                                <a href="" class="btn btn-success" data-toggle="modal" data-target="#modalStock" title="aumentar inventario"
-                                                @click.prevent="selectProducto({{$producto}}, 1)">
-                                                 <i class="bi bi-bag-plus-fill"></i></a>
-                                                 
-                                                <a href="" class="btn btn-danger" data-toggle="modal" data-target="#modalStock" title="disminuir inventario"
-                                                @click.prevent="selectProducto({{$producto}}, 2)">
-                                                 <i class="bi bi-dash-circle-fill"></i></a>
-                                            </td>
-                                        </tr>
-                                    @endforeach
+                                        <td>@{{ producto.color_producto.producto.id }}</td>
+                                        <td>@{{ producto.color_producto.producto.nombre }}</td>
+                                        <td> <a :href="'../product/' + producto.color_producto.slug">
+                                                <img :src="'../storage/' + producto.color_producto.imagenes[0].url"
+                                                    alt="" style="height: 50px; width: 50px;" class="rounded-circle">
+                                            </a>
+                                        </td>
+                                        <td>@{{ producto.talla.nombre }}</td>
+                                        <td>@{{ producto.color_producto.color.nombre }}</td>
+                                        <td>@{{ producto.stock }}</td>
+                                        <td>
+                                            <a href="" class="btn btn-success" data-toggle="modal" data-target="#modalStock" title="aumentar inventario"
+                                                @click.prevent="selectProducto(producto, 1)">
+                                                <i class="bi bi-bag-plus-fill"></i>
+                                            </a>
+                                            
+                                            <a href="" class="btn btn-danger" data-toggle="modal" data-target="#modalStock" title="disminuir inventario"
+                                                @click.prevent="selectProducto(producto, 2)">
+                                                <i class="bi bi-dash-circle-fill"></i>
+                                            </a>
+                                        </td> 
+                                    </tr> 
+
 
                                 </tbody>
                             </table>
-                            {{ $productos->appends($_GET)->links() }}
+
+                            {{-- <ul class="d-flex flex-row align-items-start justify-content-center">
+                                <li class="" v-if="pagination.current_page > 1">
+                                    <a href="#" @click.prevent="cambiarPagina(pagination.current_page - 1)">Ant</a>
+                                </li>
+                                <li v-for="page in pagesNumber" :key="page"
+                                    :class="[page == isActived ? 'active' : '']">
+                                    <a href="#" @click.prevent="cambiarPagina(page)" v-text="page"></a>
+                                </li>
+                                <li v-if="pagination.current_page < pagination.last_page">
+                                    <a class="" href="#"
+                                        @click.prevent="cambiarPagina(pagination.current_page + 1)">Sig</a>
+                                </li>
+                            </ul> --}}
+
+                            <nav>
+                                <ul class="pagination">
+
+                                    <li class="page-item disabled" v-if="pagination.current_page > 1" aria-disabled="true" aria-label="&laquo; Anterior">
+                                        <a class="page-link"
+                                            href="#"
+                                            aria-label="Anterior &lsaquo;" aria-hidden="true" @click.prevent="cambiarPagina(pagination.current_page - 1)">&lsaquo;</a>
+                                    
+                                    </li>
+
+                                    <li  class="page-item" :class="[page == isActived ? 'active' : '']" v-for="page in pagesNumber" :key="page" aria-current="page">
+                                        <a href="#" class="page-link" v-text="page" @click.prevent="cambiarPagina(page)"></a>
+                                    </li>
+
+                                    <li class="page-item" v-if="pagination.current_page < pagination.last_page">
+                                        <a class="page-link"
+                                            href="#"
+                                            rel="next" aria-label="Siguiente &raquo;" @click.prevent="cambiarPagina(pagination.current_page + 1)">&rsaquo;</a>
+                                    </li>
+                                </ul>
+                            </nav>
+
+                            {{-- {{ $productos->appends($_GET)->links() }} --}}
                         </div>
-                        <!-- /.card-body -->
+
                     </div>
-                    <!-- /.card -->
+
                 </div>
             </div>
-            <!-- /.row -->
+
         </div>
 
 
@@ -117,8 +138,10 @@
             <div class="modal-dialog" role="document">
                 <div class="modal-content">
                     <div class="modal-header modal-primary">
-                        <h5 class="modal-title" id="appModalLabel" v-text="operacion == 1 ? 'Aumentar inventario' : operacion == 2 ? 
-                            'Disminuir inventario' : 'Crear inventario nuevo'"></h5>
+                        <h5 class="modal-title" id="appModalLabel"
+                            v-text="operacion == 1 ? 'Aumentar inventario' : operacion == 2 ? 
+                            'Disminuir inventario' : 'Crear inventario nuevo'">
+                        </h5>
                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
                         </button>
@@ -128,13 +151,16 @@
 
                         <form id='formStock' class="form-horizontal" action="{{ route('stock.store') }}" method="POST">
                             @csrf
+
                             <div class="alert alert-success alert-dismissible fade show" role="alert" v-show="alertShow">
                                 <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                                     <span aria-hidden="true">&times;</span>
                                 </button>
-                                <p>{{ "Se ha actualizado el inventario del producto"}}</p>
+                                <p>{{ 'Se ha actualizado el inventario del producto' }}</p>
                             </div>
+
                             <div class="form-group row">
+
                                 <label class="col-md-3 form-control-label" for="text-input">Producto</label>
                                 <div class="col-md-9">
                                     <input type="hidden" name="operacion" v-model="operacion">
@@ -148,11 +174,6 @@
                                         @endforeach
                                     </select>
 
-                                    {{-- @if ($errors->has('producto_id'))
-                                        <small class="form-text text-danger">
-                                            {{ $errors->first('producto_id') }}
-                                        </small>
-                                    @endif --}}
 
                                     <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
                                     <span class="text-danger">
@@ -165,21 +186,13 @@
                             <div class="form-group row">
                                 <label class="col-md-3 form-control-label" for="text-input">Talla</label>
                                 <div class="col-md-9">
-                                    {{-- <select name="talla_id" id="talla_id" class="form-control">
-                                    
-                                </select> --}}
+
                                     <select name="talla_id" id="talla_id" class="form-control" v-model="talla"
                                         @change="getColores">
                                         <option value="0">Seleccione una talla</option>
                                         <option v-for="talla in arrayTallas" :key="talla.talla_id" :value="talla.talla_id"
                                             v-text="talla.nombre"></option>
                                     </select>
-
-                                    {{-- @if ($errors->has('talla_id'))
-                                        <small class="form-text text-danger">
-                                            {{ $errors->first('talla_id') }}
-                                        </small>
-                                    @endif --}}
 
                                     <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
                                     <span class="text-danger">
@@ -192,18 +205,13 @@
                             <div class="form-group row">
                                 <label class="col-md-3 form-control-label" for="text-input">Color</label>
                                 <div class="col-md-9">
-                            
+
                                     <select name="color_id" id="color_id" class="form-control" v-model="color">
                                         <option value="0">Seleccione un color</option>
-                                        <option v-for="color in arrayColores" :key="color.color_id" :value="color.color_id"
-                                            v-text="color.nombre"></option>
+                                        <option v-for="color in arrayColores" :key="color.color_id"
+                                            :value="color.color_id" v-text="color.nombre"></option>
                                     </select>
 
-                                    {{-- @if ($errors->has('color_id'))
-                                        <small class="form-text text-danger">
-                                            {{ $errors->first('color_id') }}
-                                        </small>
-                                    @endif --}}
 
                                     <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
                                     <span class="text-danger">
@@ -221,12 +229,6 @@
                                         placeholder="Cantidad" v-model="cantidad">
                                 </div>
 
-                                {{-- @if ($errors->has('cantidad'))
-                                    <small class="form-text text-danger">
-                                        {{ $errors->first('cantidad') }}
-                                    </small>
-                                @endif --}}
-
                                 <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
                                 <span class="text-danger">
                                     <strong id="cantidad-error"></strong>
@@ -234,8 +236,8 @@
 
                             </div>
 
-                            <button type="submit" class="btn btn-primary float-left" id="aceptar" @click.prevent="ingresarStock">Guardar <i
-                                class="bi bi-hdd"></i></button>
+                            <button type="submit" class="btn btn-primary float-left" id="aceptar"
+                                @click.prevent="ingresarStock">Guardar <i class="bi bi-hdd"></i></button>
                             <button type="reset" class="btn btn-danger float-right" id="rechazar">Cancelar</button>
 
                         </form>
@@ -253,79 +255,31 @@
 
 @section('scripts')
 
-    {{-- <script>
-	$(document).ready(function () {
-		
-		$.ajaxSetup({
-
-			headers: {
-				'X-CSRF-TOKEN': $("input[name= _token]").val()
-			}
-		});
-
-		
-        $(document).on('change', '#producto_id', function(e) { 
-			e.preventDefault();
-
-			var producto = parseInt($('#producto_id').val());
-
-			if (producto != 0) {
-
-				$.ajax({
-					type: "GET",
-					url: "{{ route('talla.get') }}",
-					data:{producto:producto},
-					dataType: 'json',
-					success: function (response) {
-
-						$('#talla_id').html('');
-						$('#talla_id').append('<option value="0">Seleccione una</option>')
-						
-						$.each(response.data, function (key, value) {
-							$('#talla_id').append("<option value='" 
-								+ value.talla_id + "'>" + value.nombre + "</option>");
-						});
-
-					}
-
-				});
-
-			}
-
-		});
-
-        $(document).on('change', '#talla_id', function(e) { 
-			e.preventDefault();
-
-			var producto = parseInt($('#producto_id').val());
-
-			if (producto != 0) {
-
-				$.ajax({
-					type: "GET",
-					url: "{{ route('colores.get') }}",
-					data:{producto:producto},
-					dataType: 'json',
-					success: function (response) {
-
-						$('#color_id').html('');
-						$('#color_id').append('<option value="0">Seleccione uno</option>')
-						
-						$.each(response.data, function (key, value) {
-							$('#color_id').append("<option value='" 
-								+ value.color_id + "'>" + value.nombre + "</option>");
-						});
-
-					}
-
-				});
-
-			}
-
-		});
-
-	});
-</script> --}}
-
 
 @endsection
+
+
+
+{{-- @foreach ($productos as $producto)
+    <tr>
+        <td>{{ $producto->colorProducto->producto->id }}</td>
+        <td>{{ $producto->colorProducto->producto->nombre }}</td>
+        <td> <a href="{{ route('productos.show', $producto->colorProducto->slug) }}">
+                <img src="{{ url('storage/' . $producto->colorProducto->imagenes[0]->url) }}"
+                    alt="" style="height: 50px; width: 50px;" class="rounded-circle">
+            </a>
+        </td>
+        <td>{{ $producto->talla->nombre }}</td>
+        <td>{{ $producto->colorProducto->color->nombre }}</td>
+        <td>{{ $producto->stock }}</td>
+        <td>
+            <a href="" class="btn btn-success" data-toggle="modal" data-target="#modalStock" title="aumentar inventario"
+            @click.prevent="selectProducto({{$producto}}, 1)">
+                <i class="bi bi-bag-plus-fill"></i></a>
+                
+            <a href="" class="btn btn-danger" data-toggle="modal" data-target="#modalStock" title="disminuir inventario"
+            @click.prevent="selectProducto({{$producto}}, 2)">
+                <i class="bi bi-dash-circle-fill"></i></a>
+        </td>
+    </tr>
+@endforeach --}}
